04264b58c2053d5437118def5d8796e13af99baf
Mat Ruane <mat@ruane.net.nz>
2022-06-27 21:25:55 +1200

    console log

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,6 +9,7 @@ function App() {
     getHelloWorld()
       .then((res) => {
         setHelloWorld(res.message)
+        console.log(process.env)
       })
       .catch((err) => {
         console.error(err)
