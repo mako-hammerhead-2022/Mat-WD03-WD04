913cec79072b0a4b9bb3ace5dfd0ffc55db37dd5
Mat Ruane <mat@ruane.net.nz>
2022-07-02 18:44:39 +1200

    Fixed test using mock dispatch, selector

--- client/tests/Home.test.jsx
+++ client/tests/Home.test.jsx
@@ -6,16 +6,24 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Home from '../components/Home'
 
+import { useDispatch, useSelector } from 'react-redux'
+
 require('@testing-library/jest-dom')
+
+jest.mock('react-redux')
 
 const testProducts = [
   { id: 1, name: 'Stick', description: 'Very pointy', price: 2.5 },
   { id: 2, name: 'Paperclip', description: 'Eager to help', price: 10 },
 ]
 
+const fakeDispatch = jest.fn()
+
 describe('<Home />', () => {
   it('renders a descriptive heading', () => {
-    render(<Home products={testProducts} />)
+    useDispatch.mockReturnValue(fakeDispatch)
+    useSelector.mockImplementation(() => testProducts)
+    render(<Home />)
     const headingEl = screen.getByRole('heading', {
       level: 2,
       name: /purchase/i,
@@ -24,7 +32,9 @@ describe('<Home />', () => {
   })
 
   it('renders a list of products', () => {
-    render(<Home products={testProducts} />)
+    useDispatch.mockReturnValue(fakeDispatch)
+    useSelector.mockImplementation(() => testProducts)
+    render(<Home />)
     const productListEl = screen.getByRole('list')
     expect(productListEl).toBeInTheDocument()
     expect(productListEl).toHaveClass('product-list')
